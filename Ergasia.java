4f2feaf35f855acc50aef361ebc934e9901fab9d
alexdiako <115716635+alexdiako@users.noreply.github.com>
2023-01-09 21:45:38 +0200

    Create Ergasia.java

---
+++ Ergasia.java
@@ -0,0 +1,28 @@
+package ergasia;
+
+import java.io.*;
+import java.util.*;
+
+public class Ergasia {
+
+  public static String logged;
+  //λογκντ ειναι η κατασταση 
+  public static Scanner in = new Scanner(System.in);
+  public static Map<String,String> users = new HashMap<>(), posts = new HashMap<>(), postMetadata = new HashMap<>();
+  //μαπ ειναι δομη δεδομενων, και το χασμαπ ειναι ενα ειδος δομης
+  //ποστ ειναι τα λαικς που εχει καθε ποστ
+  //ποστ ειναι τα στοιχεια του ποστ, ο χρηστης που εκανε το ποστ και ο ανγνωριστικος κωδικος
+  public static Map<String,Set<String>> postLikes = new HashMap<>();
+  //ποστλαικς εχει τον αναγνωριστικο κωδικο και μια λιστα απο χρηστες που εχουν κανει λαικ
+
+  public static void signUp(String name,String pass) {
+    if (!users.containsKey(name)) users.put(name,pass);
+    else System.out.println("Αυτό το όνομα χρησιμοποιείται ήδη.");
+  }
+
+  public static void signIn(String name,String pass) {
+    if (!users.containsKey(name)) System.out.println("Δεν υπάρχει λογαρισμός με αυτό το όνομα.");
+    else if (users.get(name).equals(pass)) logged = name;
+    else System.out.println("Λάθος στον κωδικό πρόσβασης."); 
+  }
+}
