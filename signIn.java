2e526841057ac7d9b2b327455763ea446fad6e73
alexdiako <115716635+alexdiako@users.noreply.github.com>
2023-01-15 16:51:20 +0200

    Create signIn.java

---
+++ signIn.java
@@ -0,0 +1,5 @@
+public static void signIn(String name,String pass) {
+    if (!users.containsKey(name)) System.out.println("Δεν υπάρχει λογαρισμός με αυτό το όνομα.");
+    else if (users.get(name).equals(pass)) logged = name;
+    else System.out.println("Λάθος στον κωδικό πρόσβασης."); 
+  }
